31b8f08b9357cfc161933d99b34941497984b248
parkhojeong <parkhj062@gmail.com>
2022-05-28 17:39:40 +0900

    edit popup style

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "@pages/Popup/Popup.css";
 
 const Popup = () => {
-  return <div className="App">extension popup</div>;
+  return <div className="App">commits for github</div>;
 };
 
 export default Popup;
