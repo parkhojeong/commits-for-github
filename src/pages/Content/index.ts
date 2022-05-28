479ecda2f8cf9a08d76eb13c3353ed45cdd73dde
parkhojeong <parkhj062@gmail.com>
2022-05-28 12:16:54 +0900

    addToolbarButton > 불필요한 코드 삭제

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -1,46 +1,8 @@
-import observeEl from "@src/lib/simplified-element-observer";
 import select from "select-dom";
 
-// gifs-for-github's function
-// https://github.com/N1ck/gifs-for-github/blob/c46828db015a783dec3d3209987b0eb56bcad457/src/main.js#L80
 function addToolbarButton(appendElement: HTMLElement) {
-  for (const toolbar of select.all(
-    "form:not(.ghg-has-giphy-field) markdown-toolbar"
-  )) {
-    const form = toolbar.closest("form");
-    const reviewChangesModal = toolbar.closest(
-      "#review-changes-modal .SelectMenu-modal"
-    );
-    const reviewChangesList = toolbar.closest(
-      "#review-changes-modal .SelectMenu-list"
-    );
-
-    // Add a specific class if the form is in the review changes modal, or if it is in the review changes list
-    // Otherwise the GIF selection popover will not be visible.
-    if (reviewChangesModal !== null) {
-      reviewChangesModal.classList.add("ghg-in-review-changes-modal");
-    }
-
-    if (reviewChangesList !== null) {
-      reviewChangesList.classList.add("ghg-in-review-changes-list");
-    }
-
-    // Observe the toolbars without the giphy field, add
-    // the toolbar item to any new toolbars.
-    observeEl(toolbar, () => {
-      let toolbarGroup = select.all(
-        '.toolbar-commenting > :not([class*="--hidden"]):not(button)',
-        toolbar
-      );
-      toolbarGroup = toolbarGroup[toolbarGroup.length - 1];
-
-      if (toolbarGroup) {
-        // Append the Giphy button to the toolbar
-        // cloneNode is necessary, without it, it will only be appended to the last toolbarGroup
-        toolbarGroup.append(appendElement);
-        form.classList.add("ghg-has-giphy-field");
-      }
-    });
+  for (const toolbar of select.all("markdown-toolbar")) {
+    toolbar.append(appendElement);
   }
 }
 
