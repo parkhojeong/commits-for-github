9dbccfce783b373bdd82af65832e1bd5feaee708
Jonghakseo <unqocn@gmail.com>
2022-08-07 14:47:34 +0900

    콘솔제거

--- src/pages/Content/getCommitElements.ts
+++ src/pages/Content/getCommitElements.ts
@@ -48,7 +48,6 @@ function getPRTimelineTitles(): HTMLElement[] {
       Array.from(element.getElementsByClassName("markdown-title"))
     )
     .flat(1);
-  console.log(titles);
   return titles as HTMLElement[];
 }
 
