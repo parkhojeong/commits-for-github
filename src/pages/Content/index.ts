27fbf03067b511c8a893cca26dbe2958c4d4221c
JongHak Seo <unqocn@gmail.com>
2022-10-01 00:23:50 +0900

    Update src/pages/Content/index.ts
    Co-authored-by: parkhojeong <parkhj062@gmail.com>

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -6,8 +6,7 @@ import removeAlreadyCreatedDetail from "@pages/Content/removeAlreadyCreatedDetai
 const DETAIL_CLASS_NAME = "commits-for-github-detail";
 
 const findAllToolbar = (): HTMLElement[] => {
-  const toolbars = select.all("markdown-toolbar");
-  return toolbars.filter(Boolean);
+ return select.all("markdown-toolbar").filter(Boolean);
 };
 
 const updateCommits = (htmlElement: HTMLElement): void => {
